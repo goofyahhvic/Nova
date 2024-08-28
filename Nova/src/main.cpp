0a22f9d73ab53300b334e5691c4fbbfdce280819
goofyahhvic <viniciusvmoranimations@gmail.com>
2024-08-28 09:32:25 -0300

    used new inf::Loader class in main

--- Nova/src/main.cpp
+++ Nova/src/main.cpp
@@ -5,11 +5,13 @@ namespace nova {
 	static int main(int argc, char** argv)
 	{
 		try {
-			inf::Loader::Load(INF_API_VULKAN);
-			neo::App app(argc, argv);
-			app.windows.create_window(1280, 720, "Neo-Infused Cybernetic Endeavors");
-			app.layers.create_layer<MainLayer>(5000);
-			app.run();
+			inf::Loader loader(INF_API_VULKAN);
+			{
+				neo::App app(argc, argv);
+				app.windows.create_window(1280, 720, "Neo-Infused Cybernetic Endeavors");
+				app.layers.create_layer<MainLayer>(5000);
+				app.run();
+			}
 		} catch (const std::exception& e)
 		{
 			NEO_FATAL_LOG("Exception caught in main: {0}", e.what());
