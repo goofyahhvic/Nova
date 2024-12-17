097001ec784801233719d8d08db9ebbb8d863d4f
goofyahhvic <95176727+goofyahhvic@users.noreply.github.com>
2024-12-17 20:25:58 -0300

    Updated NeoInfused

--- Nova/src/main.cpp
+++ Nova/src/main.cpp
@@ -1,14 +1,21 @@
 #include "nova_pch.hpp"
-#include "system_sets/nova_core-system-set.hpp"
 
 int main(int argc, char* argv[])
 {
-	using namespace nova;
 	try {
 		neo::app_t app;
 
-		system_set::core_t core(&app);
-		app.add_system(NEO_SYSTEMTYPE_ONEVENT, core.OnEvent);
+		std::filesystem::path shader_dir(neo::ExecDir() / "../../../NeoInfused/Influx/shaders/");
+		shader_dir = shader_dir.lexically_normal();
+
+		inf::Loader::build_shader(shader_dir / "vertex.glsl");
+		inf::Loader::build_shader(shader_dir / "fragment.glsl");
+
+		neo::app::system_set::window_t window_system_set;
+		window_system_set.AddSystems();
+
+		inf::InitializeAPI();
+		inf::window_surface_t window_surface(&window_system_set.window);
 
 		app.run();
 	} catch (const std::exception& e)
