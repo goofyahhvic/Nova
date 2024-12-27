3683da35135dcb817202330f16f131a1201e9cad
goofyahhvic <95176727+goofyahhvic@users.noreply.github.com>
2024-12-27 15:04:12 -0300

    Update NeoInfused

--- Nova/src/main.cpp
+++ Nova/src/main.cpp
@@ -4,20 +4,32 @@ int main(int argc, char* argv[])
 {
 	try {
 		neo::app_t app;
+		
+		neo::app::system_set::window_t window_system_set;
+		window_system_set.AddSystems();
+
+		inf::window_surface_t window_surface(&window_system_set.window);
 
 		std::filesystem::path shader_dir(neo::ExecDir() / "../../../NeoInfused/Influx/shaders/");
 		shader_dir = shader_dir.lexically_normal();
 
-		inf::Loader::build_shader(shader_dir / "vertex.glsl");
-		inf::Loader::build_shader(shader_dir / "fragment.glsl");
+		inf::Loader::shader_pipeline_t* pipeline = inf::Loader::create_shader_pipeline(
+			window_surface.native()
+		);
+		uint64_t vertex = inf::Loader::create_shader({ shader_dir / "vertex.glsl", INF_SHADER_STAGE_VERTEX });
+		uint64_t fragment = inf::Loader::create_shader({shader_dir / "fragment.glsl", INF_SHADER_STAGE_FRAGMENT});
 
-		neo::app::system_set::window_t window_system_set;
-		window_system_set.AddSystems();
+		inf::Loader::attach_shader_to_pipeline(pipeline, vertex);
+		inf::Loader::attach_shader_to_pipeline(pipeline, fragment);
 
-		inf::InitializeAPI();
-		inf::window_surface_t window_surface(&window_system_set.window);
+		inf::Loader::init_shader_pipeline(pipeline);
+
+		inf::Loader::destroy_shader(fragment);
+		inf::Loader::destroy_shader(vertex);
 
 		app.run();
+
+		inf::Loader::destroy_shader_pipeline(pipeline);
 	} catch (const std::exception& e)
 	{
 		NEO_FATAL_LOG("Exception caught in main: {}", e.what());
